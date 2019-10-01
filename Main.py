c5f96dbc31fcba51c350fb7f63222e0b92176ab5
Lucka <luckabarbeau@gmail.com>
2019-10-01 18:45:21 -0400

    runge kutta work et euler work tout dans main 2

--- Main.py
+++ Main.py
@@ -1,1 +1,73 @@
 # labo 4 de l'équipe 1 par Lucka Barbeau et Matthew Coffey
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def F(y,t):
+    return A_inv.dot(B.dot(y))
+
+
+M=4.2
+C=1.0
+K=100.0
+
+A=np.array([[1, 0], [-C/M,-K/M]])
+A_inv=np.linalg.inv(A)
+B=np.array([[1, 0], [C,K]])
+
+X0=10
+Y0=0
+Y0_d=-10
+
+time_step=0.001
+time_range=10
+nb_step=int(time_range/time_step)+1
+
+
+
+X=np.zeros((int(time_range/time_step)+1,1))
+X_2=np.zeros((int(time_range/time_step)+1,1))
+
+Y=np.zeros((int(time_range/time_step)+1,1))
+Y_2=np.zeros((int(time_range/time_step)+1,1))
+
+Y_d=np.zeros((int(time_range/time_step)+1,1))
+Y_d_2=np.zeros((int(time_range/time_step)+1,1))
+
+time=np.zeros((int(time_range/time_step)+1,1))
+
+time_2=np.zeros((int(time_range/time_step)+1,1))
+
+
+
+S=np.zeros((2,1))
+Y[0]=Y0
+X[0]=X0
+Y_d[0]=Y0_d
+
+S[0]=X0
+S[1]=Y0
+
+Y_2[0]=Y0
+X_2[0]=X0
+Y_d_2[0]=Y0_d
+
+
+
+t=0
+i=1
+
+while i<nb_step :
+
+    Y[i,0]=Y_d[i-1,0]*time_step+Y[i-1,0]
+    X[i,0]=Y[i-1,0]*time_step+X[i-1,0]
+    Y_d[i,0]=-K*X[i,0]/M-C*Y[i,0]/M
+
+    t+=time_step
+    time[i,0]=t
+    i+=1
+
+
+
+
+plt.plot(time,X)
---
+++ main_2.py
@@ -0,0 +1,57 @@
+# labo 4 de l'équipe 1 par Lucka Barbeau et Matthew Coffey
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def F(y,t):
+    return A_inv.dot(B.dot(y))
+
+
+M=4.2
+C=1.0
+K=100.0
+
+A=np.array([[M, 0], [0,1]])
+A_inv=np.linalg.inv(A)
+A=np.array([[C/M, K/M], [-1,0]])
+
+def F(t,y):
+    return -A.dot(y)
+def runge_kutta(F,y,t,dt):
+    k1=dt*F(t,y)
+    k2=dt*F(t+0.5*dt,y+k1*0.5)
+    k3=dt*F(t+0.5*dt,y+k2*0.5)
+    k4=dt*F(t+dt,y+k3)
+    return y+ 1/6*(k1+2*k2+2*k3+k4)
+
+X0=10
+Y0=0
+Y0_d=-10
+
+time_step=0.0001
+time_step_2=0.01
+
+time_range=10
+
+time=np.arange(0,time_range,time_step)
+time2=np.arange(0,time_range,time_step_2)
+
+y=np.array([Y0,X0])
+y2=np.array([Y0,X0])
+
+Y=[]
+Y2=[]
+
+for t in time:
+    y=y+time_step*F(t,y)
+    Y.append(y[1])
+for t2 in time2 :
+    y2=runge_kutta(F,y2,t2,time_step_2)
+    Y2.append(y2[1])
+
+
+
+
+t=[i for i in time]
+t2=[i for i in time2]
+plt.plot(t,Y,t2,Y2)
